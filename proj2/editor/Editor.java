9e329b74c0a04b6bdd394218eb9db7d6a526b71b
Vegora <wangnan0151@outlook.com>
2017-08-01 23:56:49 +0800

    Completed Editor.java

--- proj2/editor/Editor.java
+++ proj2/editor/Editor.java
@@ -1,13 +1,88 @@
 package editor;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.VPos;
+import javafx.scene.input.KeyEvent;
+import javafx.geometry.Orientation;
+
+import javafx.scene.control.ScrollBar;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 
 
 public class Editor extends Application {
-    @Override
-    public void start(Stage primaryStage) {
 
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WINDOW_HEIGHT = 500;
+
+    private class KeyEventHandler implements EventHandler<KeyEvent>{
+
+        int textCenterX;
+        int textCenterY;
+
+        private static final int STARTING_FONT_SIZE = 20;
+        private static final int STARTING_TEXT_POSITION_X = 250;
+        private static final int STARTING_TEXT_POSITION_Y = 250;
+
+        private Text displayText = new Text(STARTING_TEXT_POSITION_X, STARTING_TEXT_POSITION_Y, "");
+
+        private int fontSize = STARTING_FONT_SIZE;
+        private String fontName = "Verdana";
+
+        KeyEventHandler(final Group root, int windowWidth, int windowHeight) {
+            textCenterX = windowWidth / 2;
+            textCenterY = windowHeight / 2;
+
+            displayText = new Text(textCenterX, textCenterY, "");
+
+            displayText.setTextOrigin(VPos.TOP);
+            displayText.setFont(Font.font(fontName, fontSize));
+
+            root.getChildren().add(displayText);
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, Color.WHITE);
+
+
+        ScrollBar scrollBar = new ScrollBar();
+        scrollBar.setOrientation(Orientation.VERTICAL);
+        scrollBar.setPrefHeight(WINDOW_HEIGHT);
+
+        root.getChildren().add(scrollBar);
+
+        double usableScreenWidth = WINDOW_WIDTH - scrollBar.getLayoutBounds().getWidth();
+        scrollBar.setLayoutX(usableScreenWidth);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {

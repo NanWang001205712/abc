0abd03ff585d4037b45c8d49d672b42125d202e0
Vegora <wangnan0151@outlook.com>
2017-09-16 10:23:40 +0800

    Completed GraphDB.java

--- proj3/src/main/java/GraphDB.java
+++ proj3/src/main/java/GraphDB.java
@@ -2,6 +2,10 @@ import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -12,21 +16,89 @@ import javax.xml.parsers.SAXParserFactory;
  * @author Alan Yao
  */
 public class GraphDB {
+
+    /** List of all Connected Nodes represented in HashMap. */
+    private HashMap<Long, GraphNode> nodeList;
+    /** List of Strings mapped to a List of Nodes with name associated. */
+    private Hashtable<String, List<GraphNode>> stringNodes;
+    /** Trie of locationNames. */
+    private Trie stringLocations;
+    /** A mapping of cleaned string to its original form. */
+    private HashMap<String, String> cleanedStrings;
+
     /**
      * Example constructor shows how to create and start an XML parser.
-     * @param db_path Path to the XML file to be parsed.
+     * @param dbPath Path to the XML file to be parsed.
      */
-    public GraphDB(String db_path) {
+    public GraphDB(String dbPath) {
+        nodeList = new HashMap<>();
+        stringNodes = new Hashtable<>();
+        stringLocations = new Trie();
+        cleanedStrings = new HashMap<>();
         try {
-            File inputFile = new File(db_path);
+            File inputFile = new File(dbPath);
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
-            MapDBHandler maphandler = new MapDBHandler(this);
-            saxParser.parse(inputFile, maphandler);
+            MapDBHandler mapHandler = new MapDBHandler(this);
+            saxParser.parse(inputFile, mapHandler);
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
         clean();
+    }
+
+    /** Add a Node to NodeList. */
+    public void addNode(Long id, GraphNode node) {
+        if (!nodeList.containsKey(id)) {
+            nodeList.put(id, node);
+        }
+    }
+
+    /** Get NodeMapList. */
+    public HashMap<Long, GraphNode> getNodeMap() {
+        return nodeList;
+    }
+
+    /** Map word to Node. */
+    public void addWord(String str, GraphNode node) {
+        if (!stringNodes.containsKey(str)) {
+            ArrayList<GraphNode> container = new ArrayList<>();
+            container.add(node);
+            stringNodes.put(str, container);
+        } else {
+            List<GraphNode> copy = stringNodes.get(str);
+            if (copy.contains(node)) {
+                return;
+            }
+            copy.add(node);
+            stringNodes.put(str, copy);
+        }
+        if (!cleanedStrings.containsKey(str)) {
+            cleanedStrings.put(cleanString(str), str);
+        }
+        stringLocations.addWord(cleanString(str));
+    }
+
+    /** Returns HashTable of strings to List of Nodes. */
+    public Hashtable<String, List<GraphNode>> getStringNodes() {
+        return stringNodes;
+    }
+
+    /** Gets the location with prefix. */
+    public List<String> getLocations(String word) {
+        List<String> autoFill = stringLocations.autoComplete(word);
+        List<String> corrected = new ArrayList<>();
+        if (autoFill == null) {
+            return corrected;
+        }
+        for (String str : autoFill) {
+            corrected.add(cleanedStrings.get(str));
+        }
+        return corrected;
+    }
+
+    public String getSpecificLocation(String word) {
+        return cleanedStrings.get(cleanString(word));
     }
 
     /**
@@ -45,4 +117,7 @@ public class GraphDB {
      */
     private void clean() {
     }
+
 }
+
+
